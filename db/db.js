8cd624ac3ecc9c7a1d83fecd22dbb1a56632f470
qronix <qroniix@gmail.com>
2020-05-23 16:53:31 -0400

    Reverted DB to localhost

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-console.log('OUR DB: ', process.env.MONGO_DB);
-const url = `mongodb://${process.env.MONGO_USERNAME}
-:${process.env.MONGO_PASSWORD}
-@${process.env.MONGO_HOSTNAME}
-:${process.env.MONGO_PORT}
-/${process.env.MONGO_DB}
-?authSource=admin`;
+// const url = `mongodb://${process.env.MONGO_USERNAME}
+// :${process.env.MONGO_PASSWORD}
+// @${process.env.MONGO_HOSTNAME}
+// :${process.env.MONGO_PORT}
+// /${process.env.MONGO_DB}
+// ?authSource=admin`;
+const url = 'mongodb://localhost/Blacksmith';
 mongoose.connect(url, { useNewUrlParser: true });
 
 
